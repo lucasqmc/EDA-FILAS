e09dd75357c938798c14038106aea391ff0d63ec
Pedro Henrique Queiroz Miranda <pmiranda7410@gmail.com>
2019-10-21 01:22:05 -0300

    Implementando print e formas de visualização do vetor

--- vetor_tamanho_fixo.c
+++ vetor_tamanho_fixo.c
@@ -1,64 +1,60 @@
 #include "vetor_tamanho_fixo.h"
 
-fila teste;
-
-int filavazia(void) {
-   return teste.p >= teste.u;
-}
-int filacheia(void) {
-   return teste.u == MAX - 1;
-}
-
-void iniciaFila(){
-    teste.p = 0;
-    teste.u = 0;
-}
-
-int tiradafila(void) {
-    int retirado = teste.vetor[teste.p];
-    teste.p++;
-    
-   return retirado;
-}
-
-void colocanafila(int y) {
-   if (!filacheia()){
-     teste.vetor[teste.u++] = y;  
-   }
-   else{
-       exit(1);
-   }
-}
-
-void exibeFila(){
-    for ( int i = teste.p; i < teste.u; i++){
-        printf("%d ",teste.vetor[i]);
-    }
-    printf("\n");
-}
-
-int tamanhoFila(){
-    int count = 0;
-    for ( int i = teste.p; i < teste.u; i++){
-        count ++;
-    }
-
-    return count;
-}
-
 int main(){
-    iniciaFila();
-    colocanafila(2);
-    colocanafila(3);
-    colocanafila(4);
-    exibeFila();
-    printf("Tamanho da fila: %d\n",tamanhoFila());
-    tiradafila();
-    exibeFila();
-    printf("Tamanho da fila: %d\n",tamanhoFila());
-    colocanafila(5);
-    exibeFila();
-    printf("Tamanho da fila: %d\n",tamanhoFila());
+    // iniciaFila();
+    // exibeFila();
+    // printf("Tamanho da fila: %d\n",tamanhoFila());
+    // insereItemFila(2);
+    // exibeFila();
+    // printf("Tamanho da fila: %d\n",tamanhoFila());
+    // insereItemFila(3);
+    // exibeFila();
+    // printf("Tamanho da fila: %d\n",tamanhoFila());
+    // insereItemFila(4);
+    // exibeFila();
+    // printf("Tamanho da fila: %d\n",tamanhoFila());
+    // removeItemFila();
+    // exibeFila();
+    // printf("Tamanho da fila: %d\n",tamanhoFila());
+    // insereItemFila(5);
+    // exibeFila();
+    // printf("Tamanho da fila: %d\n",tamanhoFila());
+
+    int activity_bool = 1,option = 6, j = 0;
+
+    while(activity_bool){
+        while(option > 5 || option < 1){
+            scanf("%d", &option);
+            if(option > 5 || option < 1){
+                printf("Digite uma opção válida entre 1 - 5, por favor\n");
+            }
+        }
+        switch (option)
+        {
+            case 1:
+                insereItemFila(2);
+            break;
+
+            case 2:
+                removeItemFila();
+            break;
+
+            case 3:
+                exibeFila();
+            break;
+
+            case 4:
+                iniciaFila();
+            break;
+
+            case 5:
+                activity_bool = 0;
+            break;
+        }
+
+        option = 10;
+    }
+
 
     return 0;
 }
--- vetor_tamanho_fixo.h
+++ vetor_tamanho_fixo.h
@@ -1,6 +1,6 @@
 #include <stdlib.h>
 #include <stdio.h>
-#define MAX 40
+#define MAX 10
 
 struct Fila
 {
@@ -10,10 +10,161 @@ struct Fila
 };
 typedef struct Fila fila;
 
-int filavazia(void);
-int filacheia(void);
+fila teste;
+
+
+int filaVazia(void);
+int filaCheia(void);
 void iniciaFila();
-int tiradafila(void);
-void colocanafila(int y);
+void removeItemFila(void);
+void insereItemFila(int y);
 void exibeFila(void);
 int tamanhoFila(void);
+
+
+int filaVazia(void) {
+   return tamanhoFila() == 0;
+}
+int filaCheia(void) {
+    int bool = 0;   
+
+    if(teste.p < teste.u){
+        if(teste.u == MAX - 1 && teste.p == 0){
+            bool = 1;
+        }
+    }
+    else if(teste.u < teste.p){
+        if(teste. p - teste.u == 1){
+            bool = 1;
+        }
+    }
+
+    return bool; 
+}
+
+void iniciaFila(){
+    teste.p = 0;
+    teste.u = 0;
+}
+
+void removeItemFila(void) {
+    // int retirado = teste.vetor[teste.p];
+    // teste.p++;
+    if (!filaVazia()){
+     if(teste.p == MAX - 1){
+        teste.vetor[teste.p] = 0; 
+        teste.p = 0;
+        
+     }
+     else{
+        teste.vetor[teste.p] = 0;
+        teste.p++; 
+     }      
+   }
+   else{
+       printf("Fila Vazia! Não foi possível remover mais itens da fila\n");
+   }
+}
+
+void insereItemFila(int y) {
+   if (!filaCheia()){
+     if(teste.u == MAX - 1){
+        teste.u = 0;
+        teste.vetor[teste.u] = y; 
+     }
+     else{
+        teste.u++;
+        teste.vetor[teste.u] = y; 
+     }      
+   }
+   else{
+       printf("Fila Cheia! Não foi possível inserir item na fila\n");
+   }
+}
+
+void exibeFila(){
+
+    int tamanho_fila = tamanhoFila();
+    
+    printf(" ");
+
+    for ( int i = 0; i < MAX; i++){
+        if(i == (MAX - 1)){
+            printf("----- \n");
+        }else{
+            printf("------");
+        }
+    }
+
+    printf("|");
+
+    for ( int i = 0; i < MAX; i++){
+        if(teste.p <= teste.u){
+            if((i >= teste.p && i <= teste.u)){
+                printf(" %03d |",teste.vetor[i]);
+            }else{
+                 printf(" XXX |");
+             }
+        }
+        else if(teste.p > teste.u){
+            if((i >= 0 && i <= teste.u) || (i >= teste.p && i <= MAX)){
+                printf(" %03d |",teste.vetor[i]);
+            }else{
+                printf(" XXX |");
+             }
+        }else{
+            printf(" XXX |");
+        }
+        
+    }
+    printf("\n");
+    printf(" ");
+
+    for ( int i = 0; i < MAX; i++){
+        if(i == (MAX - 1)){
+            printf("-----");
+        }else{
+            printf("------");
+        }
+    }
+
+    printf("\n");
+    printf(">");
+
+    for ( int i = 0; i < MAX; i++){
+        if(i == teste.p || i == teste.u){
+            if(i == teste.p && i != teste.u){
+                printf("  p   ");
+            }
+            else if(teste.p == teste.u){
+                printf(" p-u  ");
+            }
+            else{
+                printf("  u   ");
+            }
+        }else{
+            printf("      ");
+        }
+    }
+    printf("\n");
+}
+
+int tamanhoFila(){
+    int count = 0;
+    if(teste.p < teste.u){
+
+        for ( int i = teste.p; i <= teste.u; i++){
+            count ++;
+        }
+    }
+    else if(teste.p > teste.u){
+        for ( int i = teste.p; i <= MAX; i++){
+            count ++;
+        }
+        for ( int i = 0; i <= teste.u; i++){
+            count ++;
+        }
+    }
+
+    return count;
+}
